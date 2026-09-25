f98d519e84d09a2768b65d5cb40b525797c61f41
Haripriya R Kusnur <86681622+haripriyarkusnur@users.noreply.github.com>
2026-09-25 21:45:36 +0530

    Create longest_common_prefix.py

---
+++ dsa/day01/longest_common_prefix.py
@@ -0,0 +1,15 @@
+# LeetCode: 14. Longest Common Prefix
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+
+        prefix = strs[0]
+
+        for word in strs:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+
+                if prefix == "":
+                    return ""
+
+        return prefix
